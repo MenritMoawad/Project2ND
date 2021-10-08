a1188aff73cc60759941cab289f4ba7e393f393f
Menrit Moawad <menrit.moawad@gmail.com>
2021-10-08 10:14:42 +0200

    server up and running

--- commentsOnlyJS/server.js
+++ commentsOnlyJS/server.js
@@ -2,17 +2,28 @@
 // Express to run server and routes
 
 // Start up an instance of app
-
+const express = require('express');
+const app = express();
 /* Dependencies */
 /* Middleware*/
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 //Here we are configuring express to use body-parser as middle-ware.
 // Cors for cross origin allowance
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
+const port = 3000;
 // Initialize the main project folder
 
 // Spin up the server
 // Callback to debug
+
+app.listen(port, function () {
+    console.log(`server up and running ... listening to port ${port}`);
+});
 
 // Initialize all route with a callback function
 
