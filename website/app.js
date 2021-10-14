abf76d774907d317941a74c88c3540bf272e8bb4
Menrit Moawad <menrit.moawad@gmail.com>
2021-10-14 08:25:42 +0200

    add alert for empty zip code

--- website/app.js
+++ website/app.js
@@ -1,5 +1,20 @@
 /* Global Variables */
-
+const apiKey = "";
+const url = "api.openweathermap.org/data/2.5/weather?zip={zip code}&appid={API key}"
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+const btn = document.querySelector("#generate");
+btn.addEventListener('click',generateFn);
+
+function generateFn() {
+    console.log("here");
+    const zipValue = document.querySelector("#zip").value;
+    if (zipValue.length===0) {
+        alert("Please enter zip code");
+    }
+    else{
+
+    }
+}
